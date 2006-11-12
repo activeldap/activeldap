372b8ccf629e8648ae5574ed4016e2b2284ef58b
Kouhei Sutou <kou@cozmixng.org>
2006-11-12 06:18:21 +0000

    * added a plugin for Rails.

---
+++ rails/plugin/active_ldap/init.rb
@@ -0,0 +1,6 @@
+require 'active_ldap'
+ActiveLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
+ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
+configurations = YAML::load(ERB.new(IO.read(ldap_configuration_file)).result)
+ActiveLdap::Base.configurations = configurations
+ActiveLdap::Base.establish_connection
