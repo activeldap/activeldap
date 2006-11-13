6df6491d3c4b1b0aed2416fdb9fd991c3194e26f
Kouhei Sutou <kou@cozmixng.org>
2006-11-13 15:16:36 +0000

    * used require_depndency instead of require.

--- rails/plugin/active_ldap/init.rb
+++ rails/plugin/active_ldap/init.rb
@@ -1,4 +1,4 @@
-require 'active_ldap'
+require_dependency 'active_ldap'
 ActiveLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
 ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
 configurations = YAML::load(ERB.new(IO.read(ldap_configuration_file)).result)
