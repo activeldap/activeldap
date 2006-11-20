091396ea6fa6a27ff320793bbe128fca962202f9
Will Drewry <wad@gmail.com>
2006-11-20 21:51:20 +0000

    Provide a dumb example of how to use the old association(return_objects) style API with the new awesome API.

--- examples/objects/user.rb
+++ examples/objects/user.rb
@@ -1,7 +1,21 @@
+require 'objects/group'
+
 class User < ActiveLdap::Base
   ldap_mapping :dn_attribute => 'uid', :prefix => 'ou=People',
                :classes => ['person', 'posixAccount']
   belongs_to :primary_group, :class => "Group",
              :foreign_key => "gidNumber", :primary_key => "gidNumber"
   belongs_to :groups, :many => 'memberUid'
+
+  # An example of using the old "return_objects" API with the
+  # new ActiveRecord-style API.
+  alias groups_mapping groups
+  def groups(return_objects=true)
+    return self.groups_mapping if return_objects
+    attr = 'cn'
+    return Group.search(:attribute => 'memberUid',
+                        :value => self.send(self.dn_attribute),
+                        :attributes => [attr]).map {|x|x[1][attr]}.flatten
+
+  end
 end
