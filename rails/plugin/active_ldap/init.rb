36fe48c13b96524ab1c83c390178c6781a1942f7
Kouhei Sutou <kou@cozmixng.org>
2007-02-03 07:42:52 +0000

    * added scaffold_al generator.

--- rails/plugin/active_ldap/init.rb
+++ rails/plugin/active_ldap/init.rb
@@ -1,6 +1,12 @@
 require_dependency 'active_ldap'
 ActiveLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
 ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
-configurations = YAML::load(ERB.new(IO.read(ldap_configuration_file)).result)
-ActiveLdap::Base.configurations = configurations
-ActiveLdap::Base.establish_connection
+if File.exist?(ldap_configuration_file)
+  configurations = YAML::load(ERB.new(IO.read(ldap_configuration_file)).result)
+  ActiveLdap::Base.configurations = configurations
+  ActiveLdap::Base.establish_connection
+else
+  message = "You should run 'script/generator scaffold_al' " +
+    "to make #{ldap_configuration_file}"
+  ActiveLdap::Base.logger.error(message)
+end
