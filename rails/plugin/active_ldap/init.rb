94a24f54c4c09ff048549aef9c643f4dbf230147
Kouhei Sutou <kou@cozmixng.org>
2007-03-09 01:23:03 +0000

    * supported RubyGems.

--- rails/plugin/active_ldap/init.rb
+++ rails/plugin/active_ldap/init.rb
@@ -1,4 +1,4 @@
-require_dependency 'active_ldap'
+require_library_or_gem 'active_ldap'
 ActiveLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
 ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
 if File.exist?(ldap_configuration_file)
