e4df1e65a05215a3e65eff52ee46a024dd86f313
Kouhei Sutou <kou@cozmixng.org>
2007-08-31 12:52:23 +0000

    * added ActiveLdap::VERSION check to Rails plugin.

--- rails/plugin/active_ldap/init.rb
+++ rails/plugin/active_ldap/init.rb
@@ -1,5 +1,14 @@
 require_library_or_gem 'active_ldap'
 ActiveLdap::Base.logger ||= RAILS_DEFAULT_LOGGER
+
+required_version = ["0", "8", "3"]
+if (ActiveLdap::VERSION.split(".") <=> required_version) < 0
+  ActiveLdap::Base.class_eval do
+    format = _("You need ActiveLdap %s or later")
+    logger.error(format % required_version.join("."))
+  end
+end
+
 ldap_configuration_file = File.join(RAILS_ROOT, 'config', 'ldap.yml')
 if File.exist?(ldap_configuration_file)
   configurations = YAML::load(ERB.new(IO.read(ldap_configuration_file)).result)
