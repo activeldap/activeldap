6d667e56a76c7fb2cf5cfbeb6238b53b47c8b093
Kouhei Sutou <kou@cozmixng.org>
2007-10-20 12:31:41 +0000

    * added menu. * supported flash-box.

--- examples/al-admin/app/helpers/application_helper.rb
+++ examples/al-admin/app/helpers/application_helper.rb
@@ -11,4 +11,19 @@ module ApplicationHelper
   def sign_up_path
     url_for(:controller => "/account", :action => "sign_up")
   end
+
+  def flash_box(message)
+    id = "flash-box"
+    fade_out = update_page {|page| page.visual_effect(:fade, id)}
+    flash_box_div = content_tag("div", message,
+                                :id => id,
+                                :onclick => fade_out)
+    set_opacity = update_page {|page| page[id].setOpacity("0.8")}
+    effect = update_page do |page|
+      page.delay(5) do
+        page.visual_effect(:fade, fade_out)
+      end
+    end
+    flash_box_div + javascript_tag(set_opacity + effect)
+  end
 end
