329a361929df1158885e986d7be976b9d593f834
Kouhei Sutou <kou@cozmixng.org>
2007-11-03 04:11:25 +0000

    * showed an objctClass detail.

--- examples/al-admin/app/controllers/object_classes_controller.rb
+++ examples/al-admin/app/controllers/object_classes_controller.rb
@@ -1,5 +1,19 @@
 class ObjectClassesController < ApplicationController
   def index
-    @object_classes = current_ldap_user.schema.object_classes
+    @object_classes = schema.object_classes
+  end
+
+  def show
+    key = params[:id]
+    @object_class = schema.object_classes.find do |object_class|
+      object_class.name == key or
+        object_class.id == key
+    end
+    raise ActiveRecord::RecordNotFound if @object_class.nil?
+  end
+
+  private
+  def schema
+    @schema ||= current_ldap_user.schema
   end
 end
