f82ecad0588ad2e0519a4f08a766b753fb723174
Kouhei Sutou <kou@cozmixng.org>
2007-11-03 05:56:15 +0000

    * added attribute page.

--- examples/al-admin/app/controllers/attributes_controller.rb
+++ examples/al-admin/app/controllers/attributes_controller.rb
@@ -2,4 +2,13 @@ class AttributesController < ApplicationController
   def index
     @attributes = schema.attributes
   end
+
+  def show
+    key = params[:id]
+    @attribute = schema.attributes.find do |attribute|
+      attribute.name == key or
+        attribute.id == key
+    end
+    raise ActiveRecord::RecordNotFound if @attribute.nil?
+  end
 end
