eb1fbae02f2b9cfb5f4d50129fc88f4469f798a5
Kouhei Sutou <kou@cozmixng.org>
2007-11-04 03:09:01 +0000

    * added help link to attribute.

--- examples/al-admin/app/helpers/attributes_helper.rb
+++ examples/al-admin/app/helpers/attributes_helper.rb
@@ -1,10 +1,15 @@
 module AttributesHelper
   include SyntaxesHelper
 
+  def attribute_url_for_options(attribute)
+    {
+      :controller => "attributes",
+      :action => "show",
+      :id => attribute
+    }
+  end
+
   def link_to_attribute(attribute)
-    link_to(h(la_(attribute)),
-            :controller => "attributes",
-            :action => "show",
-            :id => attribute)
+    link_to(h(la_(attribute)), attribute_url_for_options(attribute))
   end
 end
