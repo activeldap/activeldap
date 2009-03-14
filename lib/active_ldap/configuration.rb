45222268f80f0c87496132ee4fa2f0ead6e4d3d9
Kouhei Sutou <kou@cozmixng.org>
2009-03-14 07:02:13 +0000

    * set sasl_quiet is true by default.

--- lib/active_ldap/configuration.rb
+++ lib/active_ldap/configuration.rb
@@ -31,7 +31,7 @@ module ActiveLdap
     DEFAULT_CONFIG[:password] = nil
     DEFAULT_CONFIG[:store_password] = true
     DEFAULT_CONFIG[:allow_anonymous] = true
-    DEFAULT_CONFIG[:sasl_quiet] = false
+    DEFAULT_CONFIG[:sasl_quiet] = true
     DEFAULT_CONFIG[:try_sasl] = false
     # See http://www.iana.org/assignments/sasl-mechanisms
     DEFAULT_CONFIG[:sasl_mechanisms] = ["GSSAPI", "DIGEST-MD5",
