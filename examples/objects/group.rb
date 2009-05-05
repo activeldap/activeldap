21a598671fd62eaade6c9f8f550f0dd0c75d1cf5
Kouhei Sutou <kou@cozmixng.org>
2009-05-05 02:19:29 +0000

    * use :class_name instead of :class.

--- examples/objects/group.rb
+++ examples/objects/group.rb
@@ -6,8 +6,8 @@ class Group < ActiveLdap::Base
   # some class |:class_name| where the attributes name is
   # |:local_key|. This means that it will call
   # :class_name.new(value_of(:local_key)) to create the objects.
-  has_many :members, :class => "User", :wrap => "memberUid"
-  has_many :primary_members, :class => 'User',
+  has_many :members, :class_name => "User", :wrap => "memberUid"
+  has_many :primary_members, :class_name => 'User',
            :foreign_key => 'gidNumber',
            :primary_key => 'gidNumber'
 end # Group
--- examples/objects/user.rb
+++ examples/objects/user.rb
@@ -3,7 +3,7 @@ require 'objects/group'
 class User < ActiveLdap::Base
   ldap_mapping :dn_attribute => 'uid', :prefix => 'ou=People',
                :classes => ['person', 'posixAccount']
-  belongs_to :primary_group, :class => "Group",
+  belongs_to :primary_group, :class_name => "Group",
              :foreign_key => "gidNumber", :primary_key => "gidNumber"
   belongs_to :groups, :many => 'memberUid'
 
