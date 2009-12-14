80ec1f4e36917260dc5560d4ae15615bcf749f76
Kouhei Sutou <kou@cozmixng.org>
2009-12-14 03:10:47 +0000

    * add a test for modifying an entry with nested options.

--- test/test_base.rb
+++ test/test_base.rb
@@ -6,6 +6,18 @@ class TestBase < Test::Unit::TestCase
   include AlTestUtils
 
   priority :must
+  def test_modify_entry_with_attribute_with_nested_options
+    make_temporary_user(:simple => true) do |user,|
+      user.sn = ["Yamada",
+                 {"lang-ja" => ["山田",
+                                {"phonetic" => ["やまだ"]}]}]
+      assert_nothing_raised do
+        user.save!
+      end
+    end
+  end
+
+  priority :normal
   def test_add_entry_with_attribute_with_nested_options
     ensure_delete_user("temp-user") do |uid,|
       user = @user_class.new
@@ -26,7 +38,6 @@ class TestBase < Test::Unit::TestCase
     end
   end
 
-  priority :normal
   def test_not_rename_by_mass_update
     make_temporary_user(:simple => true) do |user,|
       original_id = user.id
