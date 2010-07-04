a498ddcac9474bc6449901d32b32ff9c989e56cb
Kouhei Sutou <kou@cozmixng.org>
2010-07-04 08:21:21 +0000

    * add vendor/locale_rails to load_path.

--- examples/al-admin/config/environment.rb
+++ examples/al-admin/config/environment.rb
@@ -37,7 +37,8 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.load_paths += %W(#{RAILS_ROOT}/../../lib)
+  config.load_paths += ["#{RAILS_ROOT}/../../lib",
+                        "#{RAILS_ROOT}/vendor/locale_rails/lib"]
   config.plugin_paths += %W(#{RAILS_ROOT}/../..)
 
   # Force all environments to use the same logger level
