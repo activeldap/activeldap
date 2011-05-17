3346448d8107c04eaad9eca529899ce2a5437dd8
Kouhei Sutou <kou@clear-code.com>
2011-05-18 01:17:15 +0900

    remove explicit ActiveSupport require.

--- lib/active_ldap.rb
+++ lib/active_ldap.rb
@@ -901,7 +901,7 @@ require_gem_if_need = Proc.new do |library_name, gem_name, *gem_args|
   end
 end
 
-require_gem_if_need.call("active_support", "activesupport", "~> 3.0.7")
+require_gem_if_need.call("active_record", "activerecord", "~> 3.0.7")
 require "active_support/core_ext"
 
 module ActiveLdap
@@ -915,7 +915,6 @@ else
   require 'active_ldap/timeout_stub'
 end
 
-require_gem_if_need.call("active_record", "activerecord", "~> 3.0.7")
 begin
   require_gem_if_need.call("locale", nil, "= 2.0.5")
   require_gem_if_need.call("fast_gettext", nil, "= 0.5.8")
