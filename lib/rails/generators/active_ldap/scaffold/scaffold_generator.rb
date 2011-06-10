a726894bb4625e8d25010f98e4d5ece3ec10f7b9
Hidetoshi Yoshimoto <hidetoshi.yoshimoto@gmail.com>
2011-06-10 18:30:40 +0900

    generators directory is moved.(-> lib/rails/generators/active_ldap) generator name is changed.(active_ldap:scaffold, active_ldap:model)

---
+++ lib/rails/generators/active_ldap/scaffold/scaffold_generator.rb
@@ -0,0 +1,14 @@
+require 'rails/generators'
+
+module ActiveLdap
+  module Generators
+class ScaffoldGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  
+  def create_ldap_yml
+    copy_file 'ldap.yml', 'config/ldap.yml'
+  end
+end
+  end
+end
+
