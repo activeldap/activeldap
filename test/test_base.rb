755df0569af3186f85de024fb25b79380481ff0f
Kouhei Sutou <kou@clear-code.com>
2011-06-14 21:40:15 +0900

    [test] @user_class's DN attribute is uid not cn.

--- test/test_base.rb
+++ test/test_base.rb
@@ -1128,12 +1128,12 @@ EOX
   end
 
   def test_to_key
-    cn = "XXX"
+    uid = "bob"
     new_user = @user_class.new
     assert_equal(nil, new_user.to_key)
-    
-    new_user.cn = cn
-    assert_equal([ new_user.dn ], new_user.to_key)
+
+    new_user.uid = uid
+    assert_equal([new_user.dn], new_user.to_key)
   end
 
   private
