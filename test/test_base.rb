58842ec0a64662951cd2996597a110cfaaaf672a
Kouhei Sutou <kou@clear-code.com>
2012-08-31 21:59:18 +0900

    test: group delete tests

--- test/test_base.rb
+++ test/test_base.rb
@@ -791,35 +791,45 @@ class TestBase < Test::Unit::TestCase
     end
   end
 
-  def test_delete
-    make_temporary_user do |user1,|
-      make_temporary_user do |user2,|
-        make_temporary_user do |user3,|
-          assert(@user_class.exists?(user1.uid))
-          @user_class.delete(user1.dn)
-          assert(!@user_class.exists?(user1.uid))
-
-          assert(@user_class.exists?(user2.dn))
-          @user_class.delete(user2.uid)
-          assert(!@user_class.exists?(user2.dn))
-
-          assert(@user_class.exists?(user3.dn))
-          @user_class.delete("uid=#{user3.uid}")
-          assert(!@user_class.exists?(user3.dn))
+  class TestDelete < self
+    class TestClass < self
+      def test_by_dn
+        make_temporary_user do |user,|
+          assert(@user_class.exists?(user.uid))
+          @user_class.delete(user.dn)
+          assert(!@user_class.exists?(user.uid))
         end
       end
-    end
 
-    make_temporary_user do |user1,|
-      make_temporary_user do |user2,|
-        make_temporary_user do |user3,|
-          assert(@user_class.exists?(user1.uid))
-          assert(@user_class.exists?(user2.uid))
-          assert(@user_class.exists?(user3.uid))
-          @user_class.delete([user1.dn, user2.uid, "uid=#{user3.uid}"])
-          assert(!@user_class.exists?(user1.uid))
-          assert(!@user_class.exists?(user2.uid))
-          assert(!@user_class.exists?(user3.uid))
+      def test_by_dn_value
+        make_temporary_user do |user,|
+          assert(@user_class.exists?(user.dn))
+          @user_class.delete(user.uid)
+          assert(!@user_class.exists?(user.dn))
+        end
+      end
+
+      def test_by_dn_attribute
+        make_temporary_user do |user,|
+          assert(@user_class.exists?(user.dn))
+          @user_class.delete("uid=#{user.uid}")
+          assert(!@user_class.exists?(user.dn))
+        end
+      end
+
+      def test_multiple
+        make_temporary_user do |user1,|
+          make_temporary_user do |user2,|
+            make_temporary_user do |user3,|
+              assert(@user_class.exists?(user1.uid))
+              assert(@user_class.exists?(user2.uid))
+              assert(@user_class.exists?(user3.uid))
+              @user_class.delete([user1.dn, user2.uid, "uid=#{user3.uid}"])
+              assert(!@user_class.exists?(user1.uid))
+              assert(!@user_class.exists?(user2.uid))
+              assert(!@user_class.exists?(user3.uid))
+            end
+          end
         end
       end
     end
