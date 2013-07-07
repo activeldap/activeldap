1e1433f67767064d9242d564c38f9fbd8c879e34
Kouhei Sutou <kou@clear-code.com>
2013-07-07 17:47:35 +0900

    test: move grouped test to the bottom

--- test/test_attributes.rb
+++ test/test_attributes.rb
@@ -71,43 +71,6 @@ class TestAttributes < Test::Unit::TestCase
                                                  {"phonetic" => ["やまだ"]}]}])
   end
 
-  class TestMassAssignment < self
-    def test_attr_protected
-      user = @user_class.new(:uid => "XXX")
-      assert_equal("XXX", user.uid)
-      user.attributes = {:uid => "ZZZ"}
-      assert_equal("XXX", user.uid)
-
-      user = @user_class.new(:sn => "ZZZ")
-      assert_equal("ZZZ", user.sn)
-
-      user = @user_class.new(:uid => "XXX", :sn => "ZZZ")
-      assert_equal("XXX", user.uid)
-      assert_equal("ZZZ", user.sn)
-
-      @user_class.attr_protected :sn
-      user = @user_class.new(:sn => "ZZZ")
-      assert_nil(user.sn)
-
-      sub_user_class = Class.new(@user_class)
-      sub_user_class.ldap_mapping :dn_attribute => "uid"
-      user = sub_user_class.new(:uid => "XXX", :sn => "ZZZ")
-      assert_equal("XXX", user.uid)
-      assert_nil(user.sn)
-
-      sub_user_class.attr_protected :cn
-      user = sub_user_class.new(:uid => "XXX", :sn => "ZZZ", :cn => "Common Name")
-      assert_equal("XXX", user.uid)
-      assert_nil(user.sn)
-      assert_nil(user.cn)
-
-      user = @user_class.new(:uid => "XXX", :sn => "ZZZ", :cn => "Common Name")
-      assert_equal("XXX", user.uid)
-      assert_nil(user.sn)
-      assert_equal("Common Name", user.cn)
-    end
-  end
-
   private
   def assert_normalize_attribute(expected, name, value)
     assert_equal(expected, ActiveLdap::Base.normalize_attribute(name, value))
@@ -187,6 +150,43 @@ class TestAttributes < Test::Unit::TestCase
       def test_false
         assert_not_blank_value(true)
       end
+    end
+  end
+
+  class TestMassAssignment < self
+    def test_attr_protected
+      user = @user_class.new(:uid => "XXX")
+      assert_equal("XXX", user.uid)
+      user.attributes = {:uid => "ZZZ"}
+      assert_equal("XXX", user.uid)
+
+      user = @user_class.new(:sn => "ZZZ")
+      assert_equal("ZZZ", user.sn)
+
+      user = @user_class.new(:uid => "XXX", :sn => "ZZZ")
+      assert_equal("XXX", user.uid)
+      assert_equal("ZZZ", user.sn)
+
+      @user_class.attr_protected :sn
+      user = @user_class.new(:sn => "ZZZ")
+      assert_nil(user.sn)
+
+      sub_user_class = Class.new(@user_class)
+      sub_user_class.ldap_mapping :dn_attribute => "uid"
+      user = sub_user_class.new(:uid => "XXX", :sn => "ZZZ")
+      assert_equal("XXX", user.uid)
+      assert_nil(user.sn)
+
+      sub_user_class.attr_protected :cn
+      user = sub_user_class.new(:uid => "XXX", :sn => "ZZZ", :cn => "Common Name")
+      assert_equal("XXX", user.uid)
+      assert_nil(user.sn)
+      assert_nil(user.cn)
+
+      user = @user_class.new(:uid => "XXX", :sn => "ZZZ", :cn => "Common Name")
+      assert_equal("XXX", user.uid)
+      assert_nil(user.sn)
+      assert_equal("Common Name", user.cn)
     end
   end
 end
