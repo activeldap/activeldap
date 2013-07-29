72609e94d3f136f588b7b15d8f4263a3c88611e2
Nowhere Man <babarocap@hotmail.com>
2013-07-29 13:57:54 +0200

    Re-encode string value to prevent encoding errors in ruby 1.9+ when saving
    ```
    c:/RailsInstaller/Ruby1.9.3/lib/ruby/gems/1.9.1/gems/activeldap-4.0.0/lib/active
    _ldap/attributes.rb:19:in `===': invalid byte sequence in UTF-8 (ArgumentError)
            from c:/RailsInstaller/Ruby1.9.3/lib/ruby/gems/1.9.1/gems/activeldap-4.0
    .0/lib/active_ldap/attributes.rb:19:in `blank_value?'
            from c:/RailsInstaller/Ruby1.9.3/lib/ruby/gems/1.9.1/gems/activeldap-4.0
    .0/lib/active_ldap/validations.rb:180:in `block in validate_ldap_values'
            from c:/RailsInstaller/Ruby1.9.3/lib/ruby/gems/1.9.1/gems/activeldap-4.0
    .0/lib/active_ldap/validations.rb:173:in `each'
            from c:/RailsInstaller/Ruby1.9.3/lib/ruby/gems/1.9.1/gems/activeldap-4.0
    .0/lib/active_ldap/validations.rb:173:in `validate_ldap_values'
    ...
    ```

--- lib/active_ldap/attributes.rb
+++ lib/active_ldap/attributes.rb
@@ -16,7 +16,7 @@ module ActiveLdap
         when Array
           value.all? {|val| blank_value?(val)}
         when String
-          /\A\s*\z/ === value
+          /\A\s*\z/ === value.try(:encode!, value.encoding,  value.encoding, :invalid => :replace)
         when true, false
           false
         when nil
