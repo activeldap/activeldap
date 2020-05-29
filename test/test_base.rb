5a1f57a6f15b0864bd33a9d99fa39b1083ef92c2
Sutou Kouhei <kou@clear-code.com>
2020-05-30 07:26:52 +0900

    test: fix indent

--- test/test_base.rb
+++ test/test_base.rb
@@ -383,7 +383,7 @@ class TestBase < Test::Unit::TestCase
     end
   end
 
-  def test_set_dn_with_unnormalized_dn_attribute_with_forward_slash 
+  def test_set_dn_with_unnormalized_dn_attribute_with_forward_slash
     make_temporary_user do |user,|
       new_dn = "uid=temp/user1,#{user.class.base}"
       assert_not_equal(user.dn.to_s, new_dn)
@@ -394,7 +394,7 @@ class TestBase < Test::Unit::TestCase
       assert_true(user.save!)
       assert_true(user.class.find(user.uid).update_attributes!(gidNumber: 100069))
     end
- end
+  end
 
   def test_destroy_with_empty_base_and_prefix_of_class
     make_temporary_user do |user,|
