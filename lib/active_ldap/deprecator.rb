e99b8147bee0e0a3eb0d2ddc5557c367debbd032
J-Verz <71394762+J-Verz@users.noreply.github.com>
2024-11-19 21:51:41 +0100

    Fix whitespace in lib/active_ldap/deprecator.rb

--- lib/active_ldap/deprecator.rb
+++ lib/active_ldap/deprecator.rb
@@ -1,5 +1,5 @@
-  module ActiveLdap
-    def self.deprecator
-      @deprecator ||= ActiveSupport::Deprecation.new("7.1.0", "ActiveLdap")
-    end
+module ActiveLdap
+  def self.deprecator
+    @deprecator ||= ActiveSupport::Deprecation.new("7.1.0", "ActiveLdap")
   end
+end
